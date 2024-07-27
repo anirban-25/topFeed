3f48effe67c28e40ca00253738e956c04abe55f1
atisha-b <ishabhattacharya45@gmail.com>
2024-07-27 18:28:58 +0530

    dash

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,21 +3,25 @@ import React from "react";
 import Image from "next/image";
 import SidePanel from '@/components/SidePanel';
 import RedditComponent from '@/components/RedditComponent';
+import DashboardHeader from '@/components/DashboardHeader';
 const page = () => {
     return (
         <div className="flex">
-        <div className="flex">
-            <div className="relative w-full -z-10 flex justify-center items-center top-0 mx-auto">
-                
+            <div className="flex h-full">
             
+                <div className=" relative w-full -z-10 flex justify-center items-center top-0 mx-auto">
+                    
+                
 
-                <SidePanel />
+                    <SidePanel />
 
-             </div>
-        </div>     
+
+                </div>
+            </div>     
            
        
         <div className="flex-grow bg-white">
+        <DashboardHeader />
         <RedditComponent />
     
         </div>
--- src/components/RedditComponent.jsx
+++ src/components/RedditComponent.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 const RedditComponent = () => {
     return (
-        <div className="p-8">
-            <h1 className="text-2xl font-bold mb-4">Create your Reddit TopFeed</h1>
-            <p className="text-gray-600 mb-8">Lorem ipsum dolor sit amet consectetur. Cras est ornare rhoncus massa nunc ornare purus aliquet massa.</p>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">+ Create New Feed</button>
+        <div className="flex flex-col items-center justify-center min-h-screen p-8">
+            <Image src="/images/reddit.png" alt="Reddit Feed" width={457} height={270} />
+            <div className="text-center mb-8 mt-8">
+                <h1 className="text-3xl font-bold mb-4">Create your Reddit TopFeed</h1>
+                <div className="max-w-lg mx-auto">
+                    <p className="text-gray-600 mb-8">Lorem ipsum dolor sit amet consectetur. Cras est ornare rhoncus massa nunc ornare purus aliquet massa.</p>
+                </div>
+                <button className="bg-[#146EF5] hover:bg-blue-900 text-white px-4 py-2 rounded-lg">+ Create New Feed</button>
+            </div>
         </div>
     );
 };
