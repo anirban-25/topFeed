fe87a5afa3e9fb3991b7bca967703593737bb7de
Anirban <anirban.ghosh.aiml25@heritageit.edu.in>
2024-09-04 09:49:49 +0530

    pushing -next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains : ["lh3.googleusercontent.com", "firebasestorage.googleapis.com", "unavatar.io", "x.com", "twitter.com"]
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+      "unavatar.io",
+      "x.com",
+      "twitter.com",
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "topfeed.ai",
+        "www.topfeed.ai",
+        "d3vmmufb5e554w.amplifyapp.com",
+      ],
     },
-    
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Adjust this in production
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
