b47b61bb3726a37eac8c3740bd06ebdaef20eea2
atisha-b <ishabhattacharya45@gmail.com>
2024-09-11 20:40:05 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    target: 'server',
     images: {
         domains : ["lh3.googleusercontent.com", "firebasestorage.googleapis.com", "unavatar.io", "x.com", "twitter.com"]
     }
