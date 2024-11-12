009a3e7a5d5df1d4ddc8ad5c0cceb9cfcdbdcd35
Anirban <anirban.ghosh.aiml25@heritageit.edu.in>
2024-11-12 12:20:53 +0530

    push

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Banner = () => {
@@ -27,7 +28,12 @@ const Banner = () => {
           className="mx-auto absolute left-0 top-0 blur-md"
         />
 
-        <Image src="/images/banner.png" height={500} width={600} className=" w-[300px] md:w-[600px] -z-10 absolute right-0 bottom-0"/>
+        <Image
+          src="/images/banner.png"
+          height={500}
+          width={600}
+          className=" w-[300px] md:w-[600px] -z-10 absolute right-0 bottom-0"
+        />
         <Image
           src="/images/circle.png"
           height={100}
@@ -37,15 +43,21 @@ const Banner = () => {
         />
         <div className="ml-2 md:ml-20 mt-10">
           <div className="text-xl md:text-5xl max-w-[40rem]  font-bold">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#FFFFFF] via-[#55A3F8] to-[#7567D9]">Customize feed&nbsp;</span>
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#FFFFFF] via-[#55A3F8] to-[#7567D9]">
+              Customize feed&nbsp;
+            </span>
             while exploring content.
           </div>
           <div className="mt-5 max-w-[30rem] text-xs md:text-sm text-[#E6E6E6]">
             Wave goodbye to hectic news finding, focus on quality content and
-            let Newsly take care of the rest!
+            let TopFeed take care of the rest!
           </div>
           <div>
-            <button className="bg-[#146EF5] px-2 p-1 mt-10 rounded-md font-semibold">Start your free trial</button>
+            <Link href="/pricing">
+              <button className="bg-[#146EF5] px-2 p-1 mt-10 rounded-md font-semibold">
+                Start your free trial
+              </button>
+            </Link>
           </div>
         </div>
       </div>
